cd48dde1c3acb0b1653cbc2f0a199ff4b223719d
Zaid EL GAOUAL <elgaoual.zaid@gmail.com>
2025-02-05 14:01:34 +0100

    supression and scss

--- BookFinder/src/app/features/books/books.module.ts
+++ BookFinder/src/app/features/books/books.module.ts
@@ -4,7 +4,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BooksRoutingModule } from './books-routing.module';
 import { BookListComponent } from './components/book-list/book-list.component';
 import { BookDetailComponent } from './components/book-detail/book-detail.component';
-import { BookSearchComponent } from './components/book-search/book-search.component';
 import { BookFavoritesComponent } from './components/book-favorites/book-favorites.component';
 import { SharedModule } from '../../shared/shared.module';
 
@@ -14,7 +13,6 @@ import { SharedModule } from '../../shared/shared.module';
   imports: [
     BookListComponent,
     BookDetailComponent,
-    BookSearchComponent,
     BookFavoritesComponent,
     CommonModule,
     BooksRoutingModule,
